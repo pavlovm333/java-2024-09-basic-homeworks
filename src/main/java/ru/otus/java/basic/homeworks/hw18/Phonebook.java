f0c3ee381e28851d67baaab35e0367fed839bbf7
pavlovmu <pavlovm333@gmail.com>
2024-12-01 16:23:48 +0300

    ДЗ_18_1

--- src/main/java/ru/otus/java/basic/homeworks/hw18/Phonebook.java
+++ src/main/java/ru/otus/java/basic/homeworks/hw18/Phonebook.java
@@ -6,7 +6,7 @@ import java.util.Map;
 import java.util.Set;
 
 public class Phonebook {
-    HashMap<String, HashSet<String>> phonebook;
+    private HashMap<String, HashSet<String>> phonebook;
 
     Phonebook() {
         phonebook = new HashMap<>();
